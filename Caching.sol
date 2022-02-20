ad4c9e67925bc72a0b2d88474311b0d74befeda7
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-02-20 21:00:28 +0530

    Note regarding caching after EIPP 2929

--- Caching.sol
+++ Caching.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.0;
 // Reading & doing operation on a memory variables(cache) is cheaper when compared to that from storage variable(non cache)
 
+//NOTE: Caching is not always true, ever since EIP-2929 the first SLOAD operation costs 2100 gas, 
+// but once that memory is read, it is cached and considered considered warm, which has a cost of 100 gas to load again.
+// It would still save gas to load and retrieve that variable in memory, especially if read more than twice.
 contract GasSaveCaching {
     uint public num = 50;
 
