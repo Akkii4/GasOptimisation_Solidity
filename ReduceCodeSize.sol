52d23e66c7737e8349989107c8deb58ff7657a55
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-07-03 21:15:26 +0530

    Update ReduceCodeSize.sol

--- ReduceCodeSize.sol
+++ ReduceCodeSize.sol
@@ -1,39 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-// Solidity has code size limit of 24576 bytes(24 Kb) thus one way is to reduce modifier to internal function
-// CodeSize can be computed by determining the length of the contract bytecode
 
-// bytecode length 670 
-contract ReducedCodeSize {
-    uint public num = 50;
-
-    function _validNum() private view {
-        require(msg.value == num);
-    }
-
-    modifier validNum {
-        _validNum();
-        _;
-    }
-
-    function test1() public validNum payable {
-        //logic
-    }
-
-    function test2() public validNum payable {
-        //logic
-    }
-
-    function test3() public validNum payable {
-        //logic
-    }
-
-    function test4() public validNum payable {
-        //logic
-    }
-}
-
-
+/**
+* Solidity has code size limit of 24576 bytes(24 Kb) thus one way is to reduce modifier to internal function
+* CodeSize can be computed by determining the length of the contract bytecode
+*/
 
 // bytecode length 750
 contract NormalCode {
@@ -59,64 +30,49 @@ contract NormalCode {
     function test4() public validNum payable {
         //logic
     }
+
+    /** here above functions compiles like :
+        function test() public payable {
+            require(msg.value == num);      // Modifier copy and paste it's code into all the functions that are accessing that modifier.
+            //logic
+        }
+    */
 }
 
+// bytecode length 670 
+contract ReducedCodeSize {
+    uint public num = 50;
 
-/* Modifier copy and paste the same code into all the functions that are accessing that modifier.
-Instead when having a function inside modifier, code is not copied but it instead makes the internal call.
-
-in NormalCode contract
-
-modifier validNum {
+    function _validNum() private view {
         require(msg.value == num);
+    }
+
+    modifier validNum {
+        _validNum();
         _;
     }
 
-function test1() public validNum payable {
-    //logic
+    
+    function test1() public validNum payable {        
+        //logic
+    }
+
+    function test2() public validNum payable {
+        //logic
+    }
+
+    function test3() public validNum payable {
+        //logic
+    }
+
+    function test4() public validNum payable {
+        //logic
+    }
+
+    /** here above functions compiles like :
+        function test() public payable {
+            _validNum();        // code is not copied but it instead makes the internal call.
+            //logic
+        }
+    */
 }
-
-function test2() public validNum payable {
-    //logic
-}
-
-// compiles to :
-
-function test1() public validNum payable {
-    require(msg.value == num);
-    //logic
-}
-
-function test2() public validNum payable {
-    require(msg.value == num);
-    //logic
-}
-
-whereas in ReducedCodeSize contract
-internal function
------------------------------------------------------------
-modifier validNum {
-    _validNum();
-    _;
-}
-
-function test1() public validNum payable {
-    //logic
-}
-
-function test2() public validNum payable {
-    //logic
-}
-
-// compiles to :
-
-function test1() public validNum payable {
-    _validNum();
-    //logic
-}
-
-function test2() public validNum payable {
-    _validNum();
-    //logic
-}
-*/
